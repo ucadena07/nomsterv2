d3a8a0075b605524282b486a2c30aa4d1e0d7b00
Ulises Cadena <ucadena07@gmail.com>
2020-02-18 08:43:45 -0600

    setting up devise

--- Gemfile
+++ Gemfile
@@ -75,3 +75,5 @@ gem 'will_paginate-bootstrap'
 gem 'simple_form'
 
 gem 'font-awesome-rails'
+
+gem 'devise'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'nomsterv2-ulises-cadena.herokuapp.com'}
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
