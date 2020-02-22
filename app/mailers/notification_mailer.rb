ff52345e7be1491aff426cd0f49566300ff3a503
Ulises Cadena <ucadena07@gmail.com>
2020-02-22 11:20:24 -0600

    Previwing th email

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
   default from: 'no-reply@nomsterapp.com'
 
   def comment_added
-    mail(to: "ucadena07@gmail.com",
+    mail(to: "cadenau4@gmail.com",
         subject: 'A comment has been added to your place')
   end
 end
